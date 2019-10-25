0e03467ed2669a98fe2834696d51559bc29081f2
janelucas <50459345+janelucas@users.noreply.github.com>
2019-10-25 15:37:42 -0700

    Update install.R

--- install.R
+++ install.R
@@ -1,3 +1,7 @@
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+BiocManager::install("biomformat", version = "1.12.0")
+
 pkgs = c("tidyverse", "metacoder")
 ncores = parallel::detectCores()
 install.packages(pkgs, Ncpus = ncores)
